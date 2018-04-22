2ec1796e404e9fa55ca586a3f0b06f3642ebadd1
Xu Qiaolun <Jamesxql@Gmail.com>
2018-04-22 20:33:27 +0800

    修改New方法的实现方式

--- errors.go
+++ errors.go
@@ -1,15 +1,8 @@
 package errors
 
+import "errors"
+
 // New returns an error that formats as the given text.
 func New(text string) error {
-	return &errorString{text}
-}
-
-// errorString is a trivial implementation of error.
-type errorString struct {
-	s string
-}
-
-func (e *errorString) Error() string {
-	return e.s
+	return errors.New(text)
 }
